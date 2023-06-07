520f471279a56bbdb94b6256d49e2422b33314ee
Miro <45430033+HotelEnglish@users.noreply.github.com>
2023-06-07 09:42:43 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>韶年华</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          揽清风，邀明月，无坚不克。
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
